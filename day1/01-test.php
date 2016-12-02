f242e6b3b723ed76e09a7b7bf21f2e8d49af4783
wangpingo <350316275@qq.com>
2016-12-02 11:41:11 +0800

    PHPStudy
    php代码提交

---
+++ day1/01-test.php
@@ -0,0 +1,13 @@
+<?php
+	
+	echo "123";
+	echo "<br/>";
+	echo "中国";
+	$a=123;
+	echo $a;
+?>
+<meta charset='UTF-8' />
+<form action="" method="">
+	year<input type="text" />
+	<input type='submit' value="提交" />
+</form>
